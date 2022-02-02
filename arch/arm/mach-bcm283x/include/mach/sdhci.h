5004bc336d5a13c54703316bbde2b4aa457daf9a
Alexander Trofimov <molochmail@gmail.com>
2022-02-02 11:04:52 +0100

    import tree.
    Signed-off-by: Alexander Trofimov <molochmail@gmail.com>

---
+++ arch/arm/mach-bcm283x/include/mach/sdhci.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * (C) Copyright 2012,2015 Stephen Warren
+ */
+
+#ifndef _BCM2835_SDHCI_H_
+#define _BCM2835_SDHCI_H_
+
+#ifndef CONFIG_BCM2835
+#ifdef CONFIG_BCM2838
+#define BCM2835_SDHCI_BASE 0xfe300000
+#else
+#define BCM2835_SDHCI_BASE 0x3f300000
+#endif
+#else
+#define BCM2835_SDHCI_BASE 0x20300000
+#endif
+
+int bcm2835_sdhci_init(u32 regbase, u32 emmc_freq);
+
+#endif
